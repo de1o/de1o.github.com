f494aa63ff3ff3878e7095cb439ccb00d0a995c9
delo <deloeating@gmail.com>
2012-01-06 11:45:47 +0800

    try some html

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Language" content="zh">
+<title>who's page</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<div id="container">
+<div class="top">
+<div id="t1" >
+
+</div>
+<div id="t2">
+
+</div>
+<div id="t3">
+<h3>待完成</h3>
+</div>
+</div>
+<div class="left">
+	<div id="l1"></div>
+	<div id="l2"></div>
+	<div id="l3"></div>
+</div>
+<div class="middle">
+	<div id="m1">
+		<h1>delo</h1>
+		<p>>我知道什么？	</p>
+	</div>
+	<div id="m2"></div>
+	<div id="m3"><a href="http://fanfou.com/delo" ><h2>饭否</h2></a></div>
+	<div id="m4"><a href="https://twitter.com/_delo" ><img src="twitterbird.png" alt="twitter" /></a></div>
+	<div id="m5"></div>
+	<div id="m6"><a href="http://www.douban.com/people/Adaiasha/" ><img src="douban.png" alt="豆瓣" /></a></div>
+	<div id="m7"></div>
+</div>
+<div class="right">
+	<div id="r1"></div>
+	<div id="r2"></div>
+	<div id="r3"></div>
+	<div id="r4"><a href="http://readnuo.tk" ><img src="blogger.png" alt="blogger" /></a></div>
+	<div id="r5"></div>
+</div>
+<div class="rright">
+	<div id="rr1"></div>
+	<div id="rr2"></div>
+</div>
+</div>
+</body>
+</html>
